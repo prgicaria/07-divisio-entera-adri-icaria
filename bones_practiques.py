2ae7d63b4bc1ca358ff07c4dff70daae4d0dd638
adri-icaria <abusquets@instituticaria.cat>
2024-10-18 09:49:26 +0200

    Primer Enviament

---
+++ bones_practiques.py
@@ -0,0 +1,8 @@
+
+Divident = int(input('introdueix lel divident'))
+Divisor = int(input('introdueix el divisor'))
+Quocient = Divident // Divisor
+Residu = Divident %  Divisor
+print(f'Divisió: {Divident}/{Divisor}')
+print(f'Quocient: {Quocient}')
+print(f'residu: {Residu}')
